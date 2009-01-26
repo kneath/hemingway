fe7087edc24c3e247b1cc668bdd275af095fc3e9
Kyle Neath <kneath@gmail.com>
2009-01-26 00:59:52 -0800

    Change the footer link back

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 			<?php
 				// You are not required to keep this link back to Warpspire, but if you wouldn't mind, leaving it in would make my day.
 			?>
-			<p class="copyright"><a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> | Theme: <a href="http://warpspire.com/hemingway" rel="designer">Hemingway</a>.</p>
+			<p class="copyright">Powered by <a href="http://warpspire.com/hemingway" rel="designer">Hemingway</a> flavored <a href="http://wordpress.org">Wordpress</a>.</p>
 			<p class="attributes"><a href="<?php bloginfo('rss2_url'); ?>">Entries RSS</a> <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments RSS</a></p>
 		</div>
 	</div>
